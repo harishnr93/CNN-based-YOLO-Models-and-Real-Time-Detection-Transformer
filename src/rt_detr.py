775122973f1e4be63598026645b16e37f2684b42
harishnr <harrish.nr@gmail.com>
2025-03-31 12:10:42 +0200

    rt_detr - src update

--- src/rt_detr.py
+++ src/rt_detr.py
@@ -3,3 +3,18 @@ Date: 26.March.2025
 Author: Harish Natarajan Ravi
 Email: harrish.nr@gmail.com
 """
+
+from ultralytics import RTDETR
+
+# Load pre-trained model
+model_path = input("Enter the RT-DETR model (e.g., 'rtdetr-l.pt'): ")
+model = RTDETR('rtdetr-l.pt')
+
+# Model information
+model.info()
+
+
+# Inference with RT-DETR model
+# results = model("inference_data\BEV_data.mp4", show=True, save=True)
+video_path = input("Enter the path to the video file (e.g., 'highway.mp4'): ")
+results = model(video_path, show=True, save=True)
